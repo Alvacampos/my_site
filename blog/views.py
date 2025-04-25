ea99e119a856d270f1df18278e13c1ca4771cd58
Gonzalo Alvarez Campos <gonzaloalvarezcampos@Gonzalos-MacBook-Pro.local>
2025-04-25 16:13:48 -0300

    more data added to the db, now loading everything from the db

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
+
 def index(request):
     latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, 'blog/index.html', {'posts': latest_posts})
@@ -13,5 +14,4 @@ def posts(request):
 
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
-
-    return render(request, 'blog/post-detail.html', {'slug': slug, 'post': post})
+    return render(request, 'blog/post-detail.html', {'slug': slug, 'post': post, 'post_tags': post.tags.all()})
